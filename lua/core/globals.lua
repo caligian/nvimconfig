fc3925734eb1dbbb91c8d3a5ce92d81b8fea41bc
Caligian <caligian@protonmail.com>
2023-02-27 18:53:19 +0530

    feat: Added light/dark support for colorschemes

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -2,7 +2,7 @@ user.lsp = user.lsp or {}
 user.plugins = user.plugins or {}
 user.shell = "/usr/bin/zsh"
 user.font = "Hack Nerd Font:h13"
-user.colorscheme = "terafox"
+user.colorscheme = { dark = "terafox", light = "base16-gruvbox-material-light-soft" }
 user.conjure_langs = {
   "clojure",
   "fennel",
