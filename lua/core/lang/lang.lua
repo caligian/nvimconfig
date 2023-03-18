77f5041722cb4a20528b4d0f86e4c510eb11c40b
Caligian <caligian@protonmail.com>
2023-03-18 08:01:33 +0530

    feat: added support for window options

--- lua/core/lang/lang.lua
+++ lua/core/lang/lang.lua
@@ -11,21 +11,21 @@ function Lang._init(self, lang, opts)
         __nonexistent = false,
         ["?hooks"] = is { "f", "t" },
         ["?bo"] = "t",
+        ["?wo"] = "t",
         ["?kbd"] = "t",
         ["?compile"] = "s",
+        ["?build"] = "s",
+        ["?test"] = "s",
         ["?linters"] = is { "s", "t" },
         ["?formatters"] = "t",
         ["?server"] = is { "s", t },
         ["?repl"] = "s",
-        ["?test"] = "s",
       },
       opts,
     },
   }
 
-  if Lang.langs[lang] then
-    return Lang.langs[lang]
-  end
+  if Lang.langs[lang] then return Lang.langs[lang] end
 
   self.name = lang
   self.autocmd = false
@@ -36,27 +36,17 @@ function Lang._init(self, lang, opts)
     else
       for _, h in ipairs(opts.hooks) do
         if is_a.t(h) then
-          utils.log_pcall(function()
-            self:hook(unpack(h))
-          end)
+          utils.log_pcall(function() self:hook(unpack(h)) end)
         else
-          utils.log_pcall(function()
-            self:hook(h)
-          end)
+          utils.log_pcall(function() self:hook(h) end)
         end
       end
     end
   end
 
-  if opts.bo then
-    self:setbufopts(opts.bo)
-  end
-  if opts.kbd then
-    self:map(unpack(opts.kbd))
-  end
-  if opts.linters then
-    opts.linters = table.tolist(opts.linters)
-  end
+  if opts.bo then self:setbufopts(opts.bo) end
+  if opts.kbd then self:map(unpack(opts.kbd)) end
+  if opts.linters then opts.linters = table.tolist(opts.linters) end
   if opts.server and is_a.s(opts.server) then
     opts.server = { name = opts.server }
   end
@@ -81,9 +71,7 @@ function Lang.hook(self, callback, opts)
 end
 
 function Lang.unhook(self, id)
-  if self.autocmd[id] then
-    self.autocmd[id]:delete()
-  end
+  if self.autocmd[id] then self.autocmd[id]:delete() end
 end
 
 function Lang.setbufopts(self, bo)
@@ -92,6 +80,19 @@ function Lang.setbufopts(self, bo)
       local bufnr = vim.fn.bufnr()
       for key, value in pairs(bo) do
         vim.api.nvim_buf_set_option(bufnr, key, value)
+      end
+    end)
+  end)
+end
+
+function Lang.setwinopts(self, wo)
+  utils.log_pcall(function()
+    self:hook(function()
+      local winid = vim.fn.bufwinid(0)
+      if winid == -1 then return end
+
+      for name, val in pairs(wo) do
+        vim.api.nvim_win_set_option(winid, name, val)
       end
     end)
   end)
@@ -112,16 +113,15 @@ function Lang.load(lang)
   local c = require("core.lang.ft." .. lang)
   local u = req("user.lang.ft." .. lang)
 
-  if not c then
-    return
-  end
+  if not c then return end
 
   return Lang(lang, table.lmerge(u or {}, c))
 end
 
 function Lang.loadall()
   return utils.log_pcall(function()
-    local src = utils.joinpath(vim.fn.stdpath "config", "lua", "core", "lang", "ft")
+    local src =
+      utils.joinpath(vim.fn.stdpath "config", "lua", "core", "lang", "ft")
     local dirs = dir.getdirectories(src)
     for _, ft in ipairs(dirs) do
       Lang.load(utils.basename(ft))
