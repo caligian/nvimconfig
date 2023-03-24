7366b70c8be6d3108bd43d05c3d5702d9c345646
Caligian <caligian@protonmail.com>
2023-03-24 22:46:12 +0530

    feat(repl): Added completion for REPL commands

--- lua/core/repl/command.lua
+++ lua/core/repl/command.lua
@@ -14,7 +14,7 @@ local function wrap(f, is_shell, start)
   return function(args)
     local ft = args.args
     if is_shell then
-      ft = 'sh'
+      ft = "sh"
     elseif #ft == 0 then
       ft = vim.bo.filetype
     end
@@ -26,43 +26,78 @@ local function wrap(f, is_shell, start)
   end
 end
 
-
+local function get_builtin()
+  return table.grep(table.keys(Lang.langs), function(ft)
+    if Lang.langs[ft].repl then
+      return true
+    end
+    return false
+  end)
+end
 
 command(
   "REPLStart",
   wrap(function(r) r:split("s", { resize = 0.3, min = 0.1 }) end, false, true),
-  {nargs=1}
+  { nargs='?', complete=get_builtin }
 )
 
-command("REPLTerminateInput", wrap(function(r) r:terminate_input() end, false, true), {})
+command(
+  "REPLTerminateInput",
+  wrap(function(r) r:terminate_input() end, false, true),
+  {complete=get_builtin, nargs='?'}
+)
 
-command("REPLStop", wrap(function(r) r:stop() end, false), {})
+command("REPLStop", wrap(function(r) r:stop() end, false), {complete=get_builtin, nargs='?'})
 
 command(
   "REPLSplit",
   wrap(function(r) r:split("s", { resize = 0.3, min = 0.1 }, false, true) end),
-  {}
+  {complete=get_builtin, nargs='?'}
 )
 
 command(
   "REPLVsplit",
   wrap(function(r) r:split("v", { resize = 0.3, min = 0.1 }, false, true) end),
-  {}
+  {complete=get_builtin, nargs='?'}
 )
 
-command("REPLDock", wrap(function(r) r:dock { relative = "win" } end, false, true), {})
+command(
+  "REPLDock",
+  wrap(function(r) r:dock { relative = "win" } end, false, true),
+  {complete=get_builtin, nargs='?'}
+)
 
-command("REPLHide", wrap(function(r) r:hide() end, false, true), {})
+command("REPLHide", wrap(function(r) r:hide() end, false, true), {complete=get_builtin, nargs='?'})
 
-command("REPLSend", wrap(function(r) r:send(vim.fn.input "To REPL > ") end, false, true), {})
+command(
+  "REPLSend",
+  wrap(function(r) r:send(vim.fn.input "To REPL > ") end, false, true),
+  {complete=get_builtin, nargs='?'}
+)
 
-command("REPLSendLine", wrap(function(r) r:send_current_line() end, false, true), {})
+command(
+  "REPLSendLine",
+  wrap(function(r) r:send_current_line() end, false, true),
+  {complete=get_builtin, nargs='?'}
+)
 
-command("REPLSendBuffer", wrap(function(r) r:send_buffer() end, false, true), {})
+command(
+  "REPLSendBuffer",
+  wrap(function(r) r:send_buffer() end, false, true),
+  {complete=get_builtin, nargs='?'}
+)
 
-command("REPLSendTillPoint", wrap(function(r) r:send_till_point() end, false, true), {})
+command(
+  "REPLSendTillPoint",
+  wrap(function(r) r:send_till_point() end, false, true),
+  {complete=get_builtin, nargs='?'}
+)
 
-command("REPLSendRange", wrap(function(r) r:send_visual_range() end, false, true), {})
+command(
+  "REPLSendRange",
+  wrap(function(r) r:send_visual_range() end, false, true),
+  {complete=get_builtin, nargs='?'}
+)
 
 -- Shell
 command(
@@ -97,7 +132,8 @@ command(
   "ShellVsplit",
   wrap(
     function(r) r:split("v", { resize = 0.3, min = 0.1, full = true }) end,
-    true, true
+    true,
+    true
   ),
   {}
 )
@@ -112,9 +148,17 @@ command(
   {}
 )
 
-command("ShellSendLine", wrap(function(r) r:send_current_line() end, true, true), {})
+command(
+  "ShellSendLine",
+  wrap(function(r) r:send_current_line() end, true, true),
+  {}
+)
 
-command("ShellSendBuffer", wrap(function(r) r:send_buffer() end, true, true), {})
+command(
+  "ShellSendBuffer",
+  wrap(function(r) r:send_buffer() end, true, true),
+  {}
+)
 
 command(
   "ShellSendTillPoint",
@@ -122,4 +166,8 @@ command(
   {}
 )
 
-command("ShellSendRange", wrap(function(r) r:send_visual_range() end, true, true), {})
+command(
+  "ShellSendRange",
+  wrap(function(r) r:send_visual_range() end, true, true),
+  {}
+)
