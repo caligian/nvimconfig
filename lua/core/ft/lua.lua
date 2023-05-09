1adb78363b0d54094b6a1b972bf7fabeb9300503
Caligian <caligian@protonmail.com>
2023-05-09 22:35:25 +0530

    refactor(filetype): single files

---
+++ lua/core/ft/lua.lua
@@ -0,0 +1,50 @@
+local package_path = vim.split(package.path, ";")
+
+return {
+  compile = "lua5.1",
+  repl = "lua5.1",
+  server = {
+    name = "lua_ls",
+    config = {
+      settings = {
+        Lua = {
+          path = package_path,
+          runtime = {
+            version = "Lua 5.1",
+          },
+          workspace = {
+            library = package_path,
+          },
+          telemetry = {
+            enable = false,
+          },
+          diagnostics = {
+            severity = { { ["undefined-global"] = false } },
+            disable = { "lowercase-global", "undefined-global" },
+          },
+        },
+      },
+    },
+  },
+  formatters = {
+    {
+      exe = "stylua",
+      args = {
+        "--call-parentheses None",
+        "--collapse-simple-statement Always",
+        "--line-endings Unix",
+        "--column-width 80",
+        "--quote-style AutoPreferDouble",
+        "--indent-type Spaces",
+        "--indent-width 2",
+        "-",
+      },
+      stdin = true,
+    },
+  },
+
+  bo = {
+    shiftwidth = 2,
+    tabstop = 2,
+  },
+}
--- lua/core/ft/sh/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-  repl = user.shell,
-  compile = user.shell,
-  server = "bashls",
-  linters = "shellcheck",
-}
